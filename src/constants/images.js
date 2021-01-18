68e8975b7e25579a799f70680b010f128f1f7cb1
Anil Patidar <anilpatidar500@gmail.com>
2021-01-18 14:14:06 +0530

    connetced with backend and app name and logo changes

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,4 +1,4 @@
-const appLogo = require('../assets/images/appLogo.png');
+const appLogo = require('../assets/images/appLogo.jpeg');
 const twitterBadge = require('../assets/images/twitter-badge.png');
 const fbBadge = require('../assets/images/fb-badge.png');
 const whatsAppBadge = require('../assets/images/whatsApp-badge.png');
